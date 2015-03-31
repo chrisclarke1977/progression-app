davidsulc/marionette-serious-progression-app

implement basic paginated view

assets/js/apps/contacts/list/list_controller.js
@@ -10,33 +10,29 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
       var contactsListPanel = new List.Panel();
 
       $.when(fetchingContacts).done(function(contacts){
-        var filteredContacts = ContactManager.Entities.FilteredCollection({
-          collection: contacts,
-          filterFunction: function(filterCriterion){
-            var criterion = filterCriterion.toLowerCase();
-            return function(contact){
-              if(contact.get("firstName").toLowerCase().indexOf(criterion) !== -1
-                || contact.get("lastName").toLowerCase().indexOf(criterion) !== -1
-                || contact.get("phoneNumber").toLowerCase().indexOf(criterion) !== -1){
-                  return contact;
-              }
-            };
-          }
-        });
-
         if(criterion){
-          filteredContacts.filter(criterion);
+          contacts.parameters.set({ criterion: criterion });
           contactsListPanel.once("show", function(){
             contactsListPanel.triggerMethod("set:filter:criterion", criterion);
           });
         }
 
-        var contactsListView = new List.Contacts({
-          collection: filteredContacts
+        contacts.goTo(1);
+        var contactsListView = new ContactManager.Common.Views.PaginatedView({
+          collection: contacts,
+          mainView: List.Contacts,
+          propagatedEvents: [
+            "itemview:contact:show",
+            "itemview:contact:edit",
+            "itemview:contact:delete"
+          ]
         });
 
         contactsListPanel.on("contacts:filter", function(filterCriterion){
-          filteredContacts.filter(filterCriterion);
+          contacts.parameters.set({
+            page: 1,
+            criterion: filterCriterion
+          });
           ContactManager.trigger("contacts:filter", filterCriterion);
         });
 

assets/js/common/views.js
@@ -37,4 +37,65 @@ ContactManager.module("Common.Views", function(Views, ContactManager, Backbone, 
       $("#spinner").spin(opts);
     }
   });
+
+  Views.PaginationControls = Marionette.ItemView.extend({
+    template: "#pagination-controls",
+    className: "pagination",
+
+    initialize: function(options){
+      this.paginatedCollection = options.paginatedCollection;
+      this.listenTo(this.paginatedCollection, "page:change:after", this.render);
+    },
+
+    events: {
+      "click a[class=navigatable]": "navigateToPage"
+    },
+
+    navigateToPage: function(e){
+      e.preventDefault();
+      var page = parseInt($(e.target).data("page"), 10);
+      this.paginatedCollection.parameters.set("page", page);
+      this.trigger("page:change", page);
+    },
+
+    serializeData: function(){
+      return this.paginatedCollection.info();
+    }
+  });
+
+  Views.PaginatedView = Marionette.Layout.extend({
+    template: "#paginated-view",
+
+    regions: {
+      paginationControlsRegion: ".js-pagination-controls",
+      paginationMainRegion: ".js-pagination-main"
+    },
+
+    initialize: function(options){
+      this.collection = options.collection;
+      var eventsToPropagate = options.propagatedEvents || [];
+
+      var controls = new Views.PaginationControls({
+        paginatedCollection: this.collection
+      });
+      var listView = new options.mainView({
+        collection: this.collection
+      });
+
+      var self = this;
+      this.listenTo(controls, "page:change", function(page){
+        self.trigger("page:change", page);
+      });
+      _.each(eventsToPropagate, function(evt){
+        self.listenTo(listView, evt, function(view, model){
+          self.trigger(evt, view, model);
+        });
+      });
+
+      this.on("show", function(){
+        this.paginationControlsRegion.show(controls);
+        this.paginationMainRegion.show(listView);
+      });
+    }
+  });
 });

chrisclarke1977/progression-app

assets/js/apps/contacts/list/list_controller.js
@@ -10,33 +10,29 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
       var contactsListPanel = new List.Panel();
 
       $.when(fetchingContacts).done(function(contacts){
-        var filteredContacts = ContactManager.Entities.FilteredCollection({
-          collection: contacts,
-          filterFunction: function(filterCriterion){
-            var criterion = filterCriterion.toLowerCase();
-            return function(contact){
-              if(contact.get("firstName").toLowerCase().indexOf(criterion) !== -1
-                || contact.get("lastName").toLowerCase().indexOf(criterion) !== -1
-                || contact.get("phoneNumber").toLowerCase().indexOf(criterion) !== -1){
-                  return contact;
-              }
-            };
-          }
-        });
-
         if(criterion){
-          filteredContacts.filter(criterion);
+          contacts.parameters.set({ criterion: criterion });
           contactsListPanel.once("show", function(){
             contactsListPanel.triggerMethod("set:filter:criterion", criterion);
           });
         }
 
-        var contactsListView = new List.Contacts({
-          collection: filteredContacts
+        contacts.goTo(1);
+        var contactsListView = new ContactManager.Common.Views.PaginatedView({
+          collection: contacts,
+          mainView: List.Contacts,
+          propagatedEvents: [
+            "childview:contact:show",
+            "childview:contact:edit",
+            "childview:contact:delete"
+          ]
         });
 
         contactsListPanel.on("contacts:filter", function(filterCriterion){
-          filteredContacts.filter(filterCriterion);
+          contacts.parameters.set({
+            page: 1,
+            criterion: filterCriterion
+          });
           ContactManager.trigger("contacts:filter", filterCriterion);
         });
 

assets/js/common/views.js
@@ -34,4 +34,65 @@ ContactManager.module("Common.Views", function(Views, ContactManager, Backbone, 
       $("#spinner").spin(opts);
     }
   });
+
+  Views.PaginationControls = Marionette.ItemView.extend({
+    template: "#pagination-controls",
+    className: "pagination",
+
+    initialize: function(options){
+      this.paginatedCollection = options.paginatedCollection;
+      this.listenTo(this.paginatedCollection, "page:change:after", this.render);
+    },
+
+    events: {
+      "click a[class=navigatable]": "navigateToPage"
+    },
+
+    navigateToPage: function(e){
+      e.preventDefault();
+      var page = parseInt($(e.target).data("page"), 10);
+      this.paginatedCollection.parameters.set("page", page);
+      this.trigger("page:change", page);
+    },
+
+    serializeData: function(){
+      return this.paginatedCollection.info();
+    }
+  });
+
+  Views.PaginatedView = Marionette.LayoutView.extend({
+    template: "#paginated-view",
+
+    regions: {
+      paginationControlsRegion: ".js-pagination-controls",
+      paginationMainRegion: ".js-pagination-main"
+    },
+
+    initialize: function(options){
+      this.collection = options.collection;
+      var eventsToPropagate = options.propagatedEvents || [];
+
+      var controls = new Views.PaginationControls({
+        paginatedCollection: this.collection
+      });
+      var listView = new options.mainView({
+        collection: this.collection
+      });
+
+      var self = this;
+      this.listenTo(controls, "page:change", function(page){
+        self.trigger("page:change", page);
+      });
+      _.each(eventsToPropagate, function(evt){
+        self.listenTo(listView, evt, function(view, model){
+          self.trigger(evt, view, model);
+        });
+      });
+
+      this.on("show", function(){
+        this.paginationControlsRegion.show(controls);
+        this.paginationMainRegion.show(listView);
+      });
+    }
+  });
 });